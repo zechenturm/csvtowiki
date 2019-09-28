c67f479a7571b66eba22e4af6c4c3d6eadd1f0c4
LVTTL <contact@lvttl.xyz>
2019-09-28 13:32:22 +0200

    test refactor: extract method to compare convert output to reference

--- csvToWIki_test.go
+++ csvToWIki_test.go
@@ -9,15 +9,17 @@ func TestCreate (t *testing.T) {
 func TestSingleCell (t *testing.T) {
 	c := Converter{}
 	out := c.Convert([]byte("a"))
-	if string(out) != "| a |" {
-		t.Fatalf("got wrong output format: wanted \"| a |\", got \"%s\"", string(out))
-	}
+	compare(t, out, "| a |")
 }
 
 func TestSmallRow (t *testing.T) {
 	c := Converter{}
 	out := c.Convert([]byte("a, b"))
-	if string(out) != "| a | b |" {
-		t.Fatalf("got wrong output format: wanted \"| a | b |\", got \"%s\"", string(out))
+	compare(t, out, "| a | b |")
+}
+
+func compare(t *testing.T,  result[]byte, reference string) {
+	if string(result) != reference {
+		t.Fatalf("got wrong output format: wanted \"%s\", got \"%s\"", reference, string(result))
 	}
 }
